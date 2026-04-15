92649887a2d2181d9ac73e1b3b9920d0c083b83d
minakshijapan77-jpg <minakshijapan77@gmail.com>
2026-04-15 12:45:38 +0530

    first13

--- backend.tf
+++ backend.tf
@@ -2,7 +2,8 @@ terraform {
   backend "s3" {
     bucket = "terraform-statebucketjenkins"
     key = "main"
-    region = "us-east-1"
+    region = "ca-central-1"
     dynamodb_table = "my-dynamodb-table"
   }
 }
+
